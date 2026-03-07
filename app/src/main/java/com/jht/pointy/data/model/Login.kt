3e5ce5acf18a230e7fd5918ad3c1b2892f2aeb70
Julie Vandenberghe <julie.vandenberghe.59@gmail.com>
2026-03-07 11:58:52 +0100

    refactor (models): split models into separate files (Login, Teacher)

--- app/src/main/java/com/jht/pointy/data/model/Login.kt
+++ app/src/main/java/com/jht/pointy/data/model/Login.kt
@@ -10,10 +10,3 @@ data class LoginResponse(
     val token: String,
     val teacher: Teacher
 )
-
-data class Teacher(
-    val id: String,
-    val firstName: String,
-    val lastName: String,
-    val email: String
-)
---
+++ app/src/main/java/com/jht/pointy/data/model/Teacher.kt
@@ -0,0 +1,8 @@
+package com.jht.pointy.data.model
+
+data class Teacher(
+    val id: String,
+    val firstName: String,
+    val lastName: String,
+    val email: String
+)
